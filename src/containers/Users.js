05c87af40a8662be34dffc66e7f02c206a655471
Michael Craig <mcraig@rpa.com>
2020-07-05 14:51:45 -0400

    Add basic React App components

---
+++ src/containers/Users.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+
+class Users extends Component {
+    render () {
+        return (
+            <div>
+                <h1>The Users</h1>
+                <p>Here are users of the course</p>
+            </div>
+        )
+    }
+}
+
+export default Pizza;
